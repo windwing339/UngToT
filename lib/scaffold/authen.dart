71ba7595e6dea598569bdcd72bfd3bdc9531e8b8
name <name@abc.com>
2019-12-23 14:25:01 +0700

    design ui

--- lib/scaffold/authen.dart
+++ lib/scaffold/authen.dart
@@ -9,8 +9,61 @@ class _AuthenState extends State<Authen> {
   // Field
 
   // Method
+  Widget mySizeBox() {
+    return SizedBox(
+      width: 5.0,
+      height: 16.0,
+    );
+  }
+
+  Widget signInButton() {
+    return RaisedButton(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+      color: Colors.blue.shade300,
+      child: Text(
+        'Sign In',
+        style: TextStyle(color: Colors.white),
+      ),
+      onPressed: () {},
+    );
+  }
+
+  Widget signUpButton() {
+    return OutlineButton(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+      child: Text('Sign Up'),
+      onPressed: () {},
+    );
+  }
+
+  Widget showButton() {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        signInButton(),
+        mySizeBox(),
+        signUpButton(),
+      ],
+    );
+  }
+
   Widget userForm() {
-    return TextFormField();
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.7,
+      child: TextFormField(
+        decoration: InputDecoration(labelText: 'User :'),
+      ),
+    );
+  }
+
+  Widget passwordForm() {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.7,
+      child: TextFormField(
+        obscureText: true,
+        decoration: InputDecoration(labelText: 'Password :'),
+      ),
+    );
   }
 
   Widget showLogo() {
@@ -37,14 +90,27 @@ class _AuthenState extends State<Authen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            showLogo(),
-            showAppName(),
-            userForm(),
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: RadialGradient(
+            colors: <Color>[Colors.white, Colors.blue.shade700],
+            radius: 1.0,
+          ),
+        ),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                showLogo(),
+                mySizeBox(),
+                showAppName(),
+                userForm(),
+                passwordForm(),
+                showButton(),
+              ],
+            ),
+          ),
         ),
       ),
     );
